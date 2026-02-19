a612ebf0e329e1d0963631d678550b4fe400e51b
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2026-02-19 14:42:15 +0100

    Update kvp.hpp

--- include/kvp.hpp
+++ include/kvp.hpp
@@ -1,3 +1,50 @@
+// KeyValueParser.hpp
+//
+//
+// DO NOT REMOVE THIS ANOTATION !
+//
+// Key Value Parser by Shadowdara
+// https://github.com/ShadowDara
+// 
+// This is a simple Key Value Parser that can be
+// used to parse key - value pairs from a string.
+//
+// 
+// Example Usage:
+// 
+// val1 = Hallo
+//
+// val2 = Wordl
+//
+// # Hallo
+// # This is a comment
+// 
+// 
+// USAGE INFO
+// 
+// - only use 'parse_kvp2', the other one is deprecated and
+// will be removed in the future.
+// - The File must be loaded externally and the whole content
+// will be passed as a string to the function, the function will
+// parse the string and return a unordered_map with the key-value pairs.
+//
+//
+// INFOS
+// 
+// KeyValueParser has comments with which lines which
+// start with '#' will be ignored.
+//
+// Only Reading is possible, writing is not implemented yet.
+//
+// PROJECT LINK!
+// https://github.com/ShadowDara/KeyValueParser
+//
+// 
+// License: MIT License Shadowdara 2026
+//
+//
+
+
 #pragma once
 
 
